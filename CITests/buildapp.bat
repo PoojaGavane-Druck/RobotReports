7f582f1f587b2205fc93a77c039cf335a8719973
MakarandDeshmukh-Druck <88836319+MakarandDeshmukh-Druck@users.noreply.github.com>
2022-01-24 17:35:19 +0530

    Updated the batch files for different repo address

--- CITests/buildapp.bat
+++ CITests/buildapp.bat
@@ -1,5 +1,5 @@
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\iarbuild.exe" c:\Jenkins\workspace\PV624_BaseStation\PV624_pipeline\EWARM\DK0499.ewp -clean DK0499
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\iarbuild.exe" c:\Jenkins\workspace\PV624_BaseStation\PV624FreeStyle\EWARM\DK0499.ewp -clean DK0499
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\iarbuild.exe" c:\Jenkins\workspace\PV624_BaseStation\PV624_pipeline\EWARM\DK0499.ewp DK0499 -log all
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\iarbuild.exe" c:\Jenkins\workspace\PV624_BaseStation\PV624FreeStyle\EWARM\DK0499.ewp DK0499 -log all
 
 EXIT
