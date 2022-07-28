e684bc71efa08504dbf8c596acd41c7f471f30b0
MakarandDeshmukh-Druck <88836319+MakarandDeshmukh-Druck@users.noreply.github.com>
2022-07-28 10:38:09 +0530

    Removed additional DK0498_1

---
+++ EWARM/copyProg.bat
@@ -0,0 +1,8 @@
+@echo off
+
+xcopy /y C:\Users\212596572\Documents\Projects\Github\PV624_SECONDARY\DK0509\EWARM\DK0509\Exe\DK0509.hex C:\Users\212596572\Documents\Projects\Github\PV624_MAIN\Programming
+xcopy /y C:\Users\212596572\Documents\Projects\Github\PV624_SECONDARY_BOOTLOADER\DK0510\EWARM\DK0510\Exe\DK0510.hex C:\Users\212596572\Documents\Projects\Github\PV624_MAIN\Programming
+xcopy /y C:\Users\212596572\Documents\Projects\Github\PV624_MAIN_BOOTLOADER\EWARM\DK0498_Bootloader\Exe\DK0498.hex C:\Users\212596572\Documents\Projects\Github\PV624_MAIN\Programming
+xcopy /y C:\Users\212596572\Documents\Projects\Github\PV624_MAIN\EWARM\DK0499\Exe\DK0499.hex C:\Users\212596572\Documents\Projects\Github\PV624_MAIN\Programming
+
+@pause
