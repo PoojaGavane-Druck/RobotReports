1a27a481a0acfda1caca1c8a55f787cbeb67cefe
MakarandDeshmukh-Druck <88836319+MakarandDeshmukh-Druck@users.noreply.github.com>
2022-08-12 11:02:03 +0530

    V00.16.02 - Corrected versioning method for the PM620 and PM620T sensors.

--- Src/cVersion.c
+++ Src/cVersion.c
@@ -25,7 +25,7 @@
 #include "main.h"
 
 /////////////////////////////////////////////////////////////////////
-#define BUILD_NUMBER 01u
+#define BUILD_NUMBER 2u
 #define MAJOR_VERSION_NUMBER 0u
 #define MINOR_VERSION_NUMBER 16u
 
