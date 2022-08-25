5885c2608ad6c2e552f7d8a09d775f0f3dbb42eb
nageswarapydisetty-druck <nageswara.pydisetty@bakerhughes.com>
2022-08-25 13:35:36 +0530

    buildapp.bat and Pragram.bat modified

--- CITests/Program.bat
+++ CITests/Program.bat
@@ -1,2 +1,2 @@
 
-"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK utility\ST-LINK_CLI.exe" -P "C:\Jenkins\workspace\PV624_BaseStation\PV624FreeStyle\EWARM\DK0499\Exe\DK0499.hex" -V
+"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK utility\ST-LINK_CLI.exe" -P "C:\Jenkins\workspace\PV624_BaseStation\PV624FreeStyle\PV624_MAIN\EWARM\DK0499\Exe\DK0499.hex" -V
