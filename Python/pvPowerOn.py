b5f7393bfd7c523f32ae1361ecc4ba85de7de40a
MakarandDeshmukh-Druck <88836319+MakarandDeshmukh-Druck@users.noreply.github.com>
2022-08-26 16:40:59 +0530

    Added power On off scripts

---
+++ Python/pvPowerOn.py
@@ -0,0 +1,52 @@
+import serial as ser
+import serial.tools.list_ports as prtlst
+import time
+
+arduinoSn = "8593731333735120E011"
+powerDown = "B"
+powerUp = "A"
+
+def findArduino(SN=[]):
+    port = {}
+    for pt in prtlst.comports():
+        print(pt.hwid)
+
+        if any(s in pt.hwid for s in SN):
+            print('\nFound ARDUINO UART:\n'+ pt.description)
+
+            port = ser.Serial(port = pt.device,
+                                baudrate = 115200,
+                                bytesize = ser.EIGHTBITS,
+                                parity =ser.PARITY_NONE,
+                                stopbits = ser.STOPBITS_ONE,
+                                xonxoff = False,
+                                rtscts = False,
+                                dsrdtr = False,
+                                timeout = 2) #note unusual parity setting for the PM COM
+            time.sleep(1) #give windows time to open the com port before flushing
+            port.flushInput()
+
+    return port 
+
+def sendMessage(port, msg):
+    port.flushInput()
+    arr = bytes(msg, 'UTF-8')
+    msg = msg + '\r\n'
+    arr = bytes(msg, 'UTF-8')
+    port.write(arr)
+    time.sleep(0.1)
+    return arr
+
+def powerOn(port):
+    message = powerUp
+    sendMessage(port, message)
+    time.sleep(2)
+
+arduinoCom = findArduino(arduinoSn)
+powerOn(arduinoCom)
+
+
+
+
+
+
