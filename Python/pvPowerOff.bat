962ce5a935040d6c377cb00da08faf9b66506234
MakarandDeshmukh-Druck <88836319+MakarandDeshmukh-Druck@users.noreply.github.com>
2022-08-26 16:56:19 +0530

    Commited with unnecessary changes

--- Python/pvPowerOff.bat
+++ Python/pvPowerOff.bat
@@ -1,1 +1,3 @@
-c:\python310\python.exe C:\Users\voramit\Documents\GitHub\PV624_MAIN\Python\pvPowerOff.py
+c:\python\python310\python.exe C:\Users\voramit\Documents\GitHub\PV624_MAIN\Python\pvPowerOff.py
+
+pause
--- Python/pvPowerOnOff.bat
+++ Python/pvPowerOnOff.bat
@@ -1,1 +1,3 @@
-c:\python310\python.exe C:\Users\voramit\Documents\GitHub\PV624_MAIN\Python\pvPowerOnOff.py
+c:\python\python310\python.exe C:\Users\voramit\Documents\GitHub\PV624_MAIN\Python\pvPowerOnOff.py
+
+pause
