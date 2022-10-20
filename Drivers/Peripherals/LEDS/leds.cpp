fbe844cf82f12bd558b5d58b80bd684a4c50b5e5
MakarandDeshmukh-Druck <88836319+MakarandDeshmukh-Druck@users.noreply.github.com>
2022-10-20 16:30:50 +0530

    V00.20.08 - Fixed bug with LED ports not being correct

--- Drivers/Peripherals/LEDS/leds.cpp
+++ Drivers/Peripherals/LEDS/leds.cpp
@@ -40,14 +40,13 @@ LEDS::LEDS()
 {
     ledOnState = GPIO_PIN_SET;
     ledOffState = GPIO_PIN_RESET;
-
     /* Battery leds */
-      
-    redPort = BAT_LEVEL5_PD8_GPIO_Port;
-    greenOnePort = BAT_LEVEL4_PC9_GPIO_Port; 
+
+    redPort = BAT_LEVEL1_PF2_GPIO_Port;
+    greenOnePort = BAT_LEVEL2_PF4_GPIO_Port; 
     greenTwoPort = BAT_LEVEL3_PF5_GPIO_Port;
-    greenThreePort = BAT_LEVEL2_PF4_GPIO_Port;
-    greenFourPort = BAT_LEVEL1_PF2_GPIO_Port;
+    greenThreePort = BAT_LEVEL4_PC9_GPIO_Port;
+    greenFourPort = BAT_LEVEL5_PD8_GPIO_Port;
            
     redPin = BAT_LEVEL1_PF2_Pin;
     greenOnePin = BAT_LEVEL2_PF4_Pin;
@@ -56,15 +55,14 @@ LEDS::LEDS()
     greenFourPin = BAT_LEVEL5_PD8_Pin;
     
     /* Status leds */
-    statusGreenPort = GPIOE;
-    statusRedPort = GPIOF;
+    statusGreenPort = GPIOF;
+    statusRedPort = GPIOE;
     statusBluePort = GPIOE;
 
     statusGreenPin = STATUS_GREEN_PF10_Pin;
     statusRedPin = STATUS_RED_PE2_Pin;
     statusBluePin = STATUS_BLUE_PE4_Pin;
 
-   
     /* Bluetooth led */
     bluetoothPort = BT_INDICATION_PE5_GPIO_Port;
     bluetoothPin = BT_INDICATION_PE5_Pin;
