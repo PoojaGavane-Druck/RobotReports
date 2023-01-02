d5fd8dfd79141dbfe6fb1b9f871400a2c9856ffb
nageswarapydisetty-druck <nageswara.pydisetty@bakerhughes.com>
2023-01-02 15:19:47 +0530

    V00.22.36: Merged from Integration Dev Fw Upgrade Branch

--- Src/cVersion.c
+++ Src/cVersion.c
@@ -25,7 +25,7 @@
 /*a*/
 /////////////////////////////////////////////////////////////////////
 
-#define BUILD_NUMBER 36u
+#define BUILD_NUMBER 37u
 #define MINOR_VERSION_NUMBER 22u
 #define MAJOR_VERSION_NUMBER 0u
 
