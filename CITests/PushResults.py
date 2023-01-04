9b2f3c90f3abcc4c2ae670c4bdad79bad9191426
212553216 <pooja.gavane@bakerhughes.com>
2023-01-04 11:45:57 +0530

    credentials updated with Pooja's

--- CITests/PushResults.py
+++ CITests/PushResults.py
@@ -3,11 +3,13 @@ import os
 import sys
 
 #https://312005216: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@github.com/PuneTeamSharing/PV624-TestAutomation.git'
+#https://312005216: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@github.com/PuneTeamSharing/PV624-TestAutomation.git'
+#https://PoojaGavane-Druck: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@github.com/PoojaGavane-Druck/DummyTestResports.git
 def push_results_onto_git(folder_with_results_name, test_repository_name,
                           HTML_Repository="https://github.build.ge.com/PuneTeamSharing/PV624TestResults.git"):
     """This method pushes the results up onto the git repsotiroy"""
     print("HTML repository is {}".format(HTML_Repository))
-    clone_command = "git clone https://312005216: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@github.com/PuneTeamSharing/PV624TestResults.git"
+    clone_command = "git clone https://PoojaGavane-Druck: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@github.com/PoojaGavane-Druck/DummyTestResports.git"
     print("Clone command is {}".format(clone_command))
     output = subprocess.check_output(clone_command, shell=True)  # Checking all the git commits
     print("Clone command output was {}".format(output))
@@ -27,13 +29,13 @@ def push_results_onto_git(folder_with_results_name, test_repository_name,
     os.system(add_to_git_repo_cmd)
 
     os.system("git commit -m \"pushing results up\"")  # Committing to the local repo
-    os.system("git push https://312005216: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@github.com/PuneTeamSharing/PV624TestResults")  # Pushing it upto the main repo
+    os.system("git push https://PoojaGavane-Druck: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@github.com/PoojaGavane-Druck/DummyTestResports.git")  # Pushing it upto the main repo
 
 
 if __name__ == '__main__':
     re_direct_folder = sys.argv[1]
     os.system("echo in the push restuls up section")
-    # os.system("dir")
+    # os.system("dir") 
     os.chdir(re_direct_folder)
     # os.system("dir")
 
